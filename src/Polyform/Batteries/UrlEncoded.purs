b9005b0cf662f82c4c1f76c59e0d81f4e0b92335
Tomasz Rybarczyk <paluho@gmail.com>
2022-01-31 13:07:10 +0100

    Add types to the main reexports

--- src/Polyform/Batteries/UrlEncoded.purs
+++ src/Polyform/Batteries/UrlEncoded.purs
@@ -2,4 +2,4 @@ module Polyform.Batteries.UrlEncoded
   ( module Types
   ) where
 
-import Polyform.Batteries.UrlEncoded.Types (Dual, Errors, Validator, fromDual, fromValidator) as Types
+import Polyform.Batteries.UrlEncoded.Types (Dual, Dual', Errors, Errors', Validator, Validator', fromDual, fromValidator) as Types
